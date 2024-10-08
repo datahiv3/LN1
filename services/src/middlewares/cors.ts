a044e5cba9688f79a7131247dad081d4e9f3b7c3
Pierre Neter <pierre@pierreneter.com>
2024-10-08 11:43:55 +0700

    chore: add cors for staging ui
    chore: add cors for staging ui

--- services/src/middlewares/cors.ts
+++ services/src/middlewares/cors.ts
@@ -2,7 +2,7 @@ import cors from "@koa/cors";
 import { isProduction, isStaging } from "../config";
 
 const crosProductionDomains = ["https://datahive.p10node.com"];
-const crosStagingDomains = ["staging-*.datahive.p10node.com"];
+const crosStagingDomains = ["https://*.datahive.p10node.com"];
 
 const isDev = !isProduction;
 
@@ -10,10 +10,17 @@ const crosDevDomains = ["http://localhost:5173"];
 
 export const corsMiddleware = cors({
   origin: (ctx): string => {
-    const validDomains = isDev ? crosDevDomains : isStaging ? crosStagingDomains : crosProductionDomains;
-    if (validDomains.indexOf(ctx.request.header.origin!) !== -1) {
-      return ctx.request.header.origin || "";
+    const allowedDomains = isDev ? crosDevDomains : isStaging ? crosStagingDomains : crosProductionDomains;
+
+    const origin = ctx.request.header.origin! || "";
+
+    for (const domain of allowedDomains) {
+      const regex = new RegExp(domain.replace(/\*/g, "[^.]+"));
+      if (regex.test(origin)) {
+        return origin;
+      }
     }
+
     return "";
   },
 
