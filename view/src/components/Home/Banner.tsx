553adbd0af84a175e28186878fdae6bc04348ea8
Pierre Neter <pierre@pierreneter.com>
2024-10-13 06:35:41 +0700

    chore: add link to DataHive Resources at banner

--- view/src/components/Home/Banner.tsx
+++ view/src/components/Home/Banner.tsx
@@ -7,7 +7,10 @@ const Banner: React.FC = () => {
       <div className="">
         The Legalese Node is a vital piece of the DataHive ecosystem, serving as the engine behind our Legal Intelligence layer. While just one component of the larger system, its
         impact is immense—transforming raw legal data into actionable intelligence. By continuously evolving to adapt to new regulations and challenges, the Legalese Node is at the
-        forefront of driving a more secure and user-centric digital experience. For more information, visit DataHive Resources.
+        forefront of driving a more secure and user-centric digital experience. For more information, visit
+        <a href="https://www.datahive.network/resources" target="_blank" rel="noopener noreferrer">
+          DataHive Resources.
+        </a>
       </div>
     </div>
   );
